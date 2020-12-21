c87db34880a81feee4ba80cfd5b610cf7dade6fc
DiegoGoethals <65620286+DiegoGoethals@users.noreply.github.com>
2020-12-21 16:04:37 +0100

    Add files via upload

---
+++ Project Scripting/cgi-bin/end_turn.cgi
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import json
+import cgi
+import game
+
+data = json.loads(cgi.FieldStorage().getvalue('data'))
+
+spel = game.Game('woorden.txt')
+spel.words = data['words']
+spel.red_team = data['red']
+spel.blue_team = data['blue']
+spel.selected = data['selected']
+spel.assassin = data['assassin']
+spel.neutral = data['neutral']
+spel.current_player = data['current_player']
+spel.code = data['code']
+spel.end_turn()
+
+with open('logs/' + spel.code + '.json', 'w') as output:
+    json.dump(spel.__dict__, output)
+
+verzenden = dict()
+
+verzenden['current_player'] = spel.current_player
+
+print("Content-Type: application/json")
+print()
+
+print(json.dumps(verzenden))
---
+++ Project Scripting/game.js
@@ -0,0 +1,261 @@
+class Game {
+
+    constructor() {
+
+        // Automatically updates the game every second
+        setInterval(function(){this.status()}.bind(this), 1000);
+
+        document.getElementById("spymaster").addEventListener("change", this.setTeamLeader.bind(this));
+
+        document.getElementById("connect").onclick = function() {
+            this.connect();
+        }.bind(this);
+
+        document.getElementById("create").onclick = function () {
+            this.start();
+        }.bind(this);
+
+        document.getElementById("end_turn").onclick = function () {
+            this.end_turn();
+        }.bind(this);
+
+        this.start();
+    }
+
+    // Fills the board so the game can be played
+    fill_board(data) {
+        this.words = data["words"];
+        this.red = data["red_team"];
+        this.blue = data["blue_team"];
+        this.neutral = data["neutral"];
+        this.assassin = data["assassin"];
+        this.code = data["code"];
+        this.current_player = data["current_player"];
+        this.winner = data["winner"];
+        this.selected = data["selected"];
+
+        // Shows code on screen so people can use it to join the game
+        document.getElementById("code").innerHTML = "Share this code: " + this.code;
+
+        // Shows current player on screen
+        const element = document.getElementById("player");
+        element.innerHTML = "Current player: " + this.current_player;
+        if (this.current_player === "red") {
+            element.style.cssText = "color: red;"
+        } else {
+            element.style.cssText = "color: blue;"
+        }
+
+        // Places words on the game board
+        const board = this.words;
+
+        document.getElementById("board").innerHTML = board
+            .map(word => `<div
+                        class="word">
+                        ${word}
+                        </div>`
+            ).join("");
+
+        // Makes all words clickable
+        document
+            .querySelectorAll(".word")
+            .forEach(word => word.addEventListener("click", this.move.bind(this)));
+    }
+
+    // Adds word to selected words and shows the color of the word on the board
+    selectWord(word) {
+        word.classList.add("selected");
+        if (this.blue.includes(word.innerText)) {
+            word.style.cssText = "background-color: blue";
+        } else if (this.red.includes(word.innerText)) {
+            word.style.cssText = "background-color: red";
+        } else if(this.neutral.includes(word.innerText)) {
+            word.style.cssText = "background-color: navajowhite;"
+        }
+    }
+
+    start() {
+
+        // Gets all properties from server to start a game
+        fetch("cgi-bin/create_game.cgi?data=").then(resp => resp.json())
+            .then(data => {
+                this.fill_board(data);
+
+                const checkbox = document.getElementById("spymaster");
+
+                if (checkbox.checked) {
+                    checkbox.checked = false;
+                    checkbox.disabled = false;
+                }
+            });
+    }
+
+    // Makes it possible to join another game
+    connect() {
+
+        let data = {
+            "code": document.getElementById("code2connect").value
+        };
+
+        fetch("cgi-bin/current_state.cgi?data=" + JSON.stringify(data)).then(resp => resp.json())
+            .then(data => {
+                this.fill_board(data);
+
+                document.querySelectorAll(".word")
+                    .forEach(word => {
+                        if (this.selected.includes(word.innerHTML)) {
+                            this.selectWord(word);
+                        }
+                    });
+            });
+    }
+
+    setWinner(winner) {
+        if (winner) {
+            alert("Congratulations " + winner + " you won the game!");
+            this.start();
+        } else {
+            const element = document.getElementById("player");
+            element.innerHTML = "Current player: " + this.current_player;
+            if (this.current_player === "red") {
+                element.style.cssText = "color: red;"
+            } else {
+                element.style.cssText = "color: blue;"
+            }
+        }
+    }
+
+    // Makes it possible to select words
+    move(event) {
+
+        const word = event.target;
+        const text = word.innerText;
+
+        if (word.classList.contains("spymaster") || word.classList.contains("selected")) {
+            return;
+        }
+
+        let data = {
+            "word": text,
+            "words": this.words,
+            "red": this.red,
+            "blue": this.blue,
+            "selected": this.selected,
+            "assassin": this.assassin,
+            "neutral": this.neutral,
+            "current_player": this.current_player,
+            "code": this.code
+        };
+
+        // Gets all properties needed to make a move from server and than makes a move
+        fetch("cgi-bin/move.cgi?data=" + JSON.stringify(data)).then(resp => resp.json())
+            .then(data => {
+                this.selected = data["selected"];
+                this.current_player = data["current_player"];
+                this.winner = data["winner"];
+
+                this.setWinner(this.winner);
+            });
+
+        this.selectWord(word);
+    }
+
+    // Makes it possible to end the turn for the current player
+    end_turn() {
+
+        const checkbox = document.getElementById("spymaster");
+
+        if (checkbox.checked) {
+            return;
+        }
+
+        let data = {
+            "words": this.words,
+            "red": this.red,
+            "blue": this.blue,
+            "selected": this.selected,
+            "assassin": this.assassin,
+            "neutral": this.neutral,
+            "current_player": this.current_player,
+            "code": this.code
+        };
+
+        // Gets data from server so the turn can be ended and ends turn
+        fetch("cgi-bin/end_turn.cgi?data=" + JSON.stringify(data)).then(resp => resp.json())
+            .then(data => {
+                this.current_player = data["current_player"];
+
+                const element = document.getElementById("player");
+                element.innerHTML = "Current player: " + this.current_player;
+                if (this.current_player === "red") {
+                    element.style.cssText = "color: red;"
+                } else {
+                    element.style.cssText = "color: blue;"
+                }
+            });
+    }
+
+    // Makes it possible to make you the spymaster
+    setTeamLeader(event) {
+
+        const checkbox = event.target;
+        if (checkbox.checked) {
+
+            document
+                .querySelectorAll(".word")
+                .forEach(word => {
+                    word.classList.add("spymaster");
+                    if (this.blue.includes(word.innerText)) {
+                        word.style.cssText = "background-color: blue";
+                    } else if (this.red.includes(word.innerText)) {
+                        word.style.cssText = "background-color: red";
+                    } else if(this.neutral.includes(word.innerText)) {
+                        word.style.cssText = "background-color: navajowhite;"
+                    }
+                });
+            checkbox.disabled = true;
+        }
+    }
+
+    // Returns the current status of the game
+    status() {
+
+        let data = {
+            "code": this.code
+        };
+
+        fetch("cgi-bin/current_state.cgi?data=" + JSON.stringify(data)).then(resp => resp.json())
+            .then(data => {
+                this.winner = data["winner"];
+                this.selected = data["selected"];
+                this.current_player = data["current_player"];
+
+                this.setWinner(this.winner);
+
+                const element = document.getElementById("player");
+                element.innerHTML = "Current player: " + this.current_player;
+                if (this.current_player === "red") {
+                    element.style.cssText = "color: red;"
+                } else {
+                    element.style.cssText = "color: blue;"
+                }
+
+                document.querySelectorAll(".word")
+                    .forEach(word => {
+                        if (this.selected.includes(word.innerText)) {
+                            this.selectWord(word);
+                        }
+
+                        if (word.classList.contains("selected") && word.classList.contains("spymaster")) {
+                            if (this.blue.includes(word.innerText)) {
+                                word.style.cssText = "background-color: blue; text-decoration: line-through;";
+                            } else if (this.red.includes(word.innerText)) {
+                                word.style.cssText = "background-color: red; text-decoration: line-through;";
+                            } else if(this.neutral.includes(word.innerText)) {
+                                word.style.cssText = "background-color: navajowhite; text-decoration: line-through;"
+                            }
+                        }
+                    });
+            });
+    }
+}
