595b8d51e3a08a8b596fc68c2aab6cc56018b1e6
Diego Goethals <diegogoethals@hotmail.nl>
2020-12-23 17:37:44 +0100

    Start and Update method for hangman finished?

--- Hangman/hangman.js
+++ Hangman/hangman.js
@@ -1,6 +1,16 @@
-class Game{
+class Game {
 
     constructor() {
+
+        setInterval(function(){this.status()}.bind(this), 1000);
+
+        document.getElementById("create").onclick = function () {
+            this.start();
+        }.bind(this);
+
+        document.getElementById("letter").onsubmit = function() {
+            this.guess();
+        }.bind(this);
 
         this.start();
     }
@@ -12,7 +22,7 @@ class Game{
         };
 
         // Gets all properties from server to start a game
-        fetch("cgi-bin/create_game.cgi?data=" +  + JSON.stringify(data)).then(resp => resp.json())
+        fetch("cgi-bin/create_game.cgi?data=" + +JSON.stringify(data)).then(resp => resp.json())
             .then(data => {
 
                 this.word = data["word"];
@@ -23,8 +33,39 @@ class Game{
                 this.mistakes_left = data["mistakes_left"];
             });
 
-        document.getElementById("word").innerText = this.word;
+        document.getElementById("word").innerHTML = this.word;
     }
 
+    guess() {
 
+        let data = {
+            "letter": document.getElementById("letter").value
+        };
+
+        fetch("cgi-bin/guess.cgi?data=" + JSON.stringify(data)).then(resp => resp.json())
+            .then(data => {
+               this.progress = data["progress"];
+               this.mistakes_left = data["mistakes*left"];
+               this.selected = data["selected"];
+               this.winner = data["winner"];
+            });
+    }
+
+    // Returns the current status of the game
+    status() {
+
+        let data = {
+            "code": this.code
+        };
+
+        fetch("cgi-bin/current_state.cgi?data=" + JSON.stringify(data)).then(resp => resp.json())
+            .then(data => {
+                this.winner = data["winner"];
+                this.selected = data["selected"];
+                this.progress = data["progress"];
+                this.mistakes_left = data["mistakes_left"];
+            });
+
+
+    }
 }
